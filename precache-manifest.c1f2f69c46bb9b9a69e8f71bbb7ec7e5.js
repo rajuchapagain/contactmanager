291250e45c335f7ffdd851669844cba322a48240
Admin <Admin@DESKTOP-6HS7OBO>
2019-11-10 20:27:46 +1100

    Updates

---
+++ precache-manifest.c1f2f69c46bb9b9a69e8f71bbb7ec7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0cd5b4cb85bead8f46093686980cb7",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "72ca4cdb293477d4e52d",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d361cc7e76cd67ac09a0",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "72ca4cdb293477d4e52d",
+    "url": "/contactmanager/static/js/2.684e199e.chunk.js"
+  },
+  {
+    "revision": "d361cc7e76cd67ac09a0",
+    "url": "/contactmanager/static/js/main.426be527.chunk.js"
+  },
+  {
+    "revision": "52a8038624ffe69f6d88",
+    "url": "/contactmanager/static/js/runtime-main.9acb0f81.js"
+  }
+]);
